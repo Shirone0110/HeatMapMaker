665cbb080e27f32ac92243b27e8d93569cd95287
Lê Trần Hoàng Lam <letranhoanglam01@gmail.com>
2021-06-08 13:41:23 -0400

    added row and column names

--- main.py
+++ main.py
@@ -70,8 +70,8 @@ def create_plot(colormap): #create plot with color map
     sns.set(style = 'white')
     #print(data)
     f, ax = plt.subplots(figsize = (4, 3))
-    sns.heatmap(vmin = 0.0, vmax = 1.0, data = data, 
-        cmap = colormap, xticklabels = columns)
+    ax = sns.heatmap(vmin = 0.0, vmax = 1.0, data = data.head(10), 
+        cmap = colormap)
     plt.yticks(rotation = 0)
     ax.xaxis.tick_top()
     ax.xaxis.set_label_position('top')
@@ -183,13 +183,12 @@ def init_BLFrame():
 
 def create_default(color): #create plot
     sns.set(style = 'white')
-    #print(data)
     f, ax = plt.subplots(figsize = (4, 3))
     if (int(color) < 3):
         cmap = sns.color_palette(values[color])
     else:
         cmap = sns.color_palette(values[color], as_cmap = True)
-    ax = sns.heatmap(data, cmap = cmap, center = 0, xticklabels = columns)
+    ax = sns.heatmap(data.head(10), cmap = cmap, center = 0)
     plt.yticks(rotation = 0)
     ax.xaxis.tick_top()
     ax.xaxis.set_label_position('top')
@@ -214,28 +213,66 @@ def init_TLFrame():
     label.grid(column = 0, row = 0, columnspan = 3, sticky = "nw")
 
     #some random color schemes
-    global values
+    global values, vDefaultColor
     values = {"1": "pastel", "2": "hls", "3": "Blues", "4": "YlOrBr",
             "5": "icefire"}
 
-    v = tk.StringVar()
-    v.set("1")
-    draw(v)
+    vDefaultColor = tk.StringVar()
+    vDefaultColor.set("1")
     count = 0
 
     for val, text in values.items():
         count += 1
-        tk.Radiobutton(TLFrame, text = text, variable = v, 
-            command = lambda v = v: draw(v), 
+        tk.Radiobutton(TLFrame, text = text, variable = vDefaultColor, 
+            command = lambda v = vDefaultColor: draw(v), 
             value = val).grid(row = count, column = 0, sticky = "nw")
 
 def init_BRFrame():
     canvas = tk.Canvas(BRFrame, width = 400, height = 300)
     canvas.grid(row = 0, column = 0)
 
+def find_top(df):
+    df['mean'] = df.mean(axis = 1)
+    df = df.sort_values(by = ['mean'], ascending = False)
+    return df.head(10)
+
+def rowNames(indices, labels):
+    rows = []
+    for index in indices:
+        rows.append(labels[index + 3])
+    return rows
+
+def process_file():
+    path = csvfile.name
+    df = pd.read_csv(path)
+    df = edit(df)
+    column = columnNames(df)
+    rows = []
+    
+    #for ind in range(4, len(df.index)):
+    for ind in range(4, 24):
+        if vYesNo.get() == 0:
+            new = noZeros(df, ind)
+        else:
+            new = inclZeros(df, ind)
+        rows.append(new)
+    
+    global data
+    data = pd.DataFrame(rows, columns = column)
+    data = find_top(data)
+    data['ind'] = rowNames(data.index, df.columns)
+    data.set_index(keys = 'ind', inplace = True)
+
+    print(data)
+    draw(vDefaultColor)
+
 def next_btn():
     SetupFrame.grid_remove()
     HeatMapFrame.grid()
+    if csvfile is None:
+        messagebox.showerror("Error", "Please choose data file")
+        return
+    process_file()
 
 def choose_file():
     files = [('CSV', '*.csv')]
@@ -247,21 +284,25 @@ def choose_file():
     addr = addr.split('/')
     showfilename.config(text = addr[-1])
 
+#calculates averages for one row not including zeros
 def noZeros(df, ind):
     sum = 0
     cnt = 0
     new = []
     lastSpec = df.at[0, "Category"]
     numOtus = df.at[0, "numOtus"]
-    
+
     for row in df.itertuples():
         if(row[2] == lastSpec):
             sum += row[ind]
             if(row[ind] != 0):
                 cnt += 1
         else:
-            avgOcurrence = sum / (cnt * numOtus)
-            new.append(avgOcurrence)
+            if(cnt != 0):
+                avgOccurrence = sum / (cnt * numOtus)
+            else:
+                avgOccurrence = 0
+            new.append(avgOccurrence)
 
             sum = 0
             cnt = 0
@@ -269,30 +310,34 @@ def noZeros(df, ind):
             if(row[ind] != 0):
                 cnt += 1
         lastSpec = row[2]
-        
+
     return new
 
+#calculates averages for one row including zeros
 def inclZeros(df, ind):
     sum = 0
     cnt = 0
     new = []
     lastSpec = df.at[0, "Category"]
     numOtus = df.at[0, "numOtus"]
-    
+
     for row in df.itertuples():
         if(row[2] == lastSpec):
             sum += row[ind]
             cnt += 1
         else:
-            avgOcurrence = sum / (cnt * numOtus)
-            new.append(avgOcurrence)
+            if(cnt != 0):
+                avgOccurrence = sum / (cnt * numOtus)
+            else:
+                avgOccurrence = 0
+            new.append(avgOccurrence)
 
             sum = 0
             cnt = 0
             sum += row[ind]
             cnt += 1
         lastSpec = row[2]
-        
+
     return new
 
 def columnNames(df):
@@ -316,24 +361,6 @@ def edit(df):
     df = df.loc[df["Category"] != "moss"] 
     return df
 
-def process_file():
-    path = csvfile.name
-    df = pd.read_csv(path)
-    df = edit(df)
-    columns = columnNames(df)
-    data = []
-    
-    for ind in range(4, 10):
-        if vYesNo.get() == 0:
-            new = noZeros(df, ind)
-        else:
-            new = inclZeros(df, ind)
-        data.append(new)
-    
-    #print(columns)
-    #for row in data:
-    #    print(row)
-
 def init_SetupFrame():
     #choose file prompt
     tk.Label(SetupFrame, text = "Choose data file").grid(row = 0, column = 0, 
@@ -352,10 +379,10 @@ def init_SetupFrame():
 
     vYesNo = tk.IntVar()
     tk.Radiobutton(SetupFrame, text = "Yes", variable = vYesNo, 
-        value = 1, command = process_file).grid(row = 2, column = 1, sticky = "nw")
+        value = 1).grid(row = 2, column = 1, sticky = "nw")
     tk.Radiobutton(SetupFrame, text = "No", variable = vYesNo,
-        value = 0, command = process_file).grid(row = 3, column = 1, sticky = "nw")
-    #vYesNo.set(0)
+        value = 0).grid(row = 3, column = 1, sticky = "nw")
+    vYesNo.set(0)
 
     next = tk.Button(SetupFrame, text = "Next", command = next_btn)
     next.grid(row = 4, column = 1, pady = 10)
@@ -389,9 +416,6 @@ def on_closing():
 sns.set()
 root = tk.Tk()
 root.wm_title("HeatMap Maker")
-
-data = np.random.rand(10, 10)
-columns = np.random.rand(10)
 
 HeatMapFrame = Frame(root)
 HeatMapFrame.grid(row = 0, column = 0, sticky = "nswe")
